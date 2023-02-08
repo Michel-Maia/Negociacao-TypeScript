371af14ef0a08ccf83b64c0d4968ea7bd115ec03
Michel Maia <programador.michel.maia@gmail.com>
2023-02-08 20:33:50 -0300

    commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,38 @@
+
+/*
+Não pode ser modificada depois de criada
+Obrigatoriamente tem uma data, quantidade e valor
+Seu volume é calculado multiplicando-se a quantidade negociada no dia pelo valor negociado
+*/
+
+export class Negociacao {
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+    get data() {
+        return this.#data; 
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor; 
+    }
+
+}
+
+
+
